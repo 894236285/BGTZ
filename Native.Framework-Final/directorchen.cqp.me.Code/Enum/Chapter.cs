c2fadc9906e0dc38e04f9f502b3f99c6c703d148
Sweet1Chen <894236285@qq.com>
2020-03-02 18:02:54 +0800

    project create
    创建项目

---
+++ Native.Framework-Final/directorchen.cqp.me.Code/Enum/Chapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace directorchen.cqp.me.Code.Enum
+{
+    public class Chapter
+    {
+        /// <summary>
+        /// 章节名称
+        /// </summary>
+        public string ChapterName { get; set; }
+
+        /// <summary>
+        /// 发布时间
+        /// </summary>
+        public string ChapterTime { get; set; }
+
+        /// <summary>
+        /// 字数
+        /// </summary>
+        public int WordNumber { get; set; }
+    }
+}
---
+++ Native.Framework-Final/directorchen.cqp.me.UI/Menu_OpenWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Native.Sdk.Cqp.EventArgs;
+using Native.Sdk.Cqp.Interface;
+
+namespace directorchen.cqp.me.UI
+{
+    public class Menu_OpenWindow : IMenuCall
+    {
+        private MainWindow _mainWindow = null;
+
+        /// <summary>
+        /// 打开窗体按钮被按下
+        /// </summary>
+        /// <param name="sender">事件来源</param>
+        /// <param name="e">事件参数</param>
+        public void MenuCall(object sender, CQMenuCallEventArgs e)
+        {
+            if (this._mainWindow == null)
+            {
+                this._mainWindow = new MainWindow();
+                this._mainWindow.Closing += MainWindow_Closing;
+                this._mainWindow.Show();	// 显示窗体
+            }
+            else
+            {
+                this._mainWindow.Activate();	// 将窗体调制到前台激活
+            }
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // 对变量置 null, 因为被关闭的窗口无法重复显示
+            this._mainWindow = null;
+        }
+    }
+}
